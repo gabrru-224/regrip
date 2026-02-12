4349af6e073281716dda5ec965b4d80c5d2626ae
gabrru-224 <gouravsharma98099@gmail.com>
2026-02-13 01:32:37 +0530

    fix smtp

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,8 +4,9 @@ const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 
 const transporter = nodemailer.createTransport({
-
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
