d9e875a66a7626ecfbcb6abb612157498f834823
gabrru-224 <gouravsharma98099@gmail.com>
2026-02-13 02:19:37 +0530

    render final final

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -16,8 +16,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  family: 4,
-  localAddress: '0.0.0.0'
 });
 
 exports.generateOtp = async (email) => {
